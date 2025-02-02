6ddf713406c34dffa7b1e3d8678ef09bfe9f880a
Harold Hernandez <harold.hernandez@visma.com>
2025-02-02 21:04:20 +0100

    heading parse initial impl

--- md/heading.py
+++ md/heading.py
@@ -1,5 +1,6 @@
 
 
 class Heading:
-    def __init__(self, parent):
-        pass
+    def __init__(self, text, level):
+        print(f"HEADING:text: {text}")
+        print(f"HEADING:level: {level}")
--- md/md.py
+++ md/md.py
@@ -1,9 +1,26 @@
 
+import re
+from heading import Heading
+
 class MD:
     def __init__(self, file_content):
-        pass
+        headings = self._find_headings(file_content)
+
+
+    def _find_headings(self, file_content):
+        headings = []
+        for line in file_content.splitlines():
+            if line.startswith('#'):     
+                match = re.search("^[#]{1,}", line)
+                groups = re.split(match.group(), line)
+                text_list = filter(lambda line: len(line) > 0, groups)
+                if len(text_list) == 1:
+                    headings.append(Heading(text_list[0].trim(), match.group()))
+
+        return headings
+
         # find top heading
-        
+
         # find metadata
         # if no root heading, find multiple same-level heading
 
